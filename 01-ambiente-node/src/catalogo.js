98f467c53150c2b8206a131696707f16541db67f
lucaslima <lucas.m.lima6@edu.senai.br>
2026-07-31 13:29:50 -0300

    Aula01 - Configuração do Ambiente Node.js

---
+++ 01-ambiente-node/src/catalogo.js
@@ -0,0 +1,29 @@
+const livro1 = {
+  titulo: "Clean Code",
+  autor: "Robert C. Martin",
+  preco: 89.9,
+  estoque: 12,
+};
+
+const livro2 = {
+  titulo: "Eloquent JavaScript",
+  autor: "Marijn Haverbeke",
+  preco: 75.0,
+  estoque: 20,
+};
+
+const livro3 = {
+    titulo: "Biblia de Gutenberg",
+    autor: "Gutenberg",
+    preco: 10000.0,
+    estoque: 1,
+};
+
+const livro4 = {
+    titulo: "Kylan Dictador",
+    autor: "Mbappe",
+    preco: 1000.0,
+    estoque: 2,
+};
+
+module.exports = { livro1, livro2, livro3, livro4 };
---
+++ 01-ambiente-node/src/index.js
@@ -0,0 +1,23 @@
+const catalogo = require("./catalogo");
+
+console.log("=== CATÁLOGO DA LIVRARIA ===");
+console.log("");
+
+console.log("Titulo: " + catalogo.livro1.titulo);
+console.log("Autor: " + catalogo.livro1.autor);
+console.log("Preco: R$ " + catalogo.livro1.preco);
+console.log("");
+
+console.log("Titulo: " + catalogo.livro2.titulo);
+console.log("Autor: " + catalogo.livro2.autor);
+console.log("Preco: R$ " + catalogo.livro2.preco);
+console.log("");
+
+console.log("Titulo: " + catalogo.livro3.titulo);
+console.log("Autor: " + catalogo.livro3.autor);
+console.log("Preco: R$ " + catalogo.livro3.preco);
+console.log("");
+
+console.log("Titulo: " + catalogo.livro4.titulo);
+console.log("Autor: " + catalogo.livro4.autor);
+console.log("Preco: R$ " + catalogo.livro4.preco);
