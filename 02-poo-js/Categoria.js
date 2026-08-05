34b31c422e3ab5a7430bdba892d0184c224bb65c
lucaslima <lucas.m.lima6@edu.senai.br>
2026-08-05 12:13:25 -0300

    feat: baixa no estoque e total de livros

--- 02-poo-js/Categoria.js
+++ 02-poo-js/Categoria.js
@@ -1,11 +1,17 @@
 class Categoria {
   #nome;
   #descricao;
+  #totalDeLivros;
   constructor(nome, descricao) {
+    this.#totalDeLivros = 0;
     this.#nome = nome;
     this.#descricao = descricao;
   }
   // ----- LEITURA (getters) -----
+  get totalDeLivros() {
+    return this.#totalDeLivros;
+  }
+
   get nome() {
     return this.#nome;
   }
@@ -20,11 +26,23 @@ class Categoria {
     }
     this.#nome = novoNome;
   }
+
+  set descricao(novaDescricao) {
+    if (novaDescricao.length < 5) {
+        console.log("ERRO: a descricao precisa de pelo menos 5 letras.");
+        return;
+    }
+    this.#descricao = novaDescricao;
+
+  }
   // ----- MÉTODO -----
   descrever() {
     console.log("Categoria: " + this.#nome);
     console.log("Descricao: " + this.#descricao);
+    console.log("Total de Livros: " + this.#totalDeLivros)
     // A saída deve ficar: Descricao: livros sobre programacao
   }
+
+  
 }
 module.exports = Categoria;
--- 02-poo-js/Livro.js
+++ 02-poo-js/Livro.js
@@ -1,40 +1,49 @@
 class Livro {
-    #preco;
-    #estoque;
+  #preco;
+  #estoque;
 
-    constructor(titulo, autor, preco, estoque) {
-        this.titulo = titulo;
-        this.autor = autor;
-        this.#preco = preco;
-        this.#estoque = estoque;
-    }
+  constructor(titulo, autor, preco, estoque) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.#preco = preco;
+    this.#estoque = estoque;
+  }
 
-    descrever() {
-        console.log("Titulo: " + this.titulo);
-        console.log("Autor: " + this.autor);
-        console.log("Preco: " + this.#preco);
-        console.log("Estoque: " + this.#estoque + " unidades");
-    }
+  descrever() {
+    console.log("Titulo: " + this.titulo);
+    console.log("Autor: " + this.autor);
+    console.log("Preco: " + this.#preco);
+    console.log("Estoque: " + this.#estoque + " unidades");
+  }
 
-    valorEmEstoque() {
-        return this.preco * this.estoque;
-    }
+  valorEmEstoque() {
+    return this.preco * this.estoque;
+  }
 
-    get preco() {
-        return this.#preco;
-    }
+  get preco() {
+    return this.#preco;
+  }
 
-    get estoque() {
-        return this.#estoque;
-    }
+  get estoque() {
+    return this.#estoque;
+  }
 
-    set preco(novoPreco) {
-        if (novoPreco < 0) {
-            console.log("ERRO: preco nao pode ser negativo. Valor recusado.");
-            return;
-        }
-        this.#preco = novoPreco;
+  set preco(novoPreco) {
+    if (novoPreco < 0) {
+      console.log("ERRO: preco nao pode ser negativo. Valor recusado.");
+      return;
     }
+    this.#preco = novoPreco;
+  }
+
+  vender(quantidade) {
+    if (quantidade > this.#estoque) {
+      console.log("ERRO: estoque insuficiente.");
+      return;
+    }
+    this.#estoque = this.#estoque - quantidade;
+    console.log("Venda registrada. Restam " + this.#estoque + " unidades.");
+  }
 }
 
-module.exports = Livro;     
+module.exports = Livro;
